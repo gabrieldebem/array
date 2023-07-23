e5aa2641da802ebf7bf62ecc9c17b7f3feaa0d69
Gabriel de Bem <gabrieldebem72@gmail.com>
2023-07-23 17:36:12 -0300

    lint

--- array_func_test.go
+++ array_func_test.go
@@ -9,7 +9,7 @@ import (
 func TestFilterWithInt(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	odd := array.Filter[int](numbers, func(value int) bool {
-		return value % 2 == 0
+		return value%2 == 0
 	})
 
 	if len(odd) != 4 {
